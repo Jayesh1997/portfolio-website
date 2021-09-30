5a53fdd8438293480ee98f7575d92383eb8575d2
Jayesh Amodkar <jayeshdamodkar@gmail.com>
2021-09-30 18:47:02 +0530

    changed folder structure

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('partials/homePg', { title: 'Home'});
+  res.render('homePg', { title: 'Home'});
 });
 
 /* GET Home page. */
 router.get('/home', function(req, res, next) {
-  res.render('partials/homePg', { title: 'Home'});
+  res.render('homePg', { title: 'Home'});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('partials/about', { title: 'About'});
+  res.render('about', { title: 'About'});
 });
 
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('partials/projects', { title: 'Projects'});
+  res.render('projects', { title: 'Projects'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('partials/service', { title: 'Services'});
+  res.render('service', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('partials/contact', { title: 'Contact'});
+  res.render('contact', { title: 'Contact'});
 });
 
 module.exports = router;
